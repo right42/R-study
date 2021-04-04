93796468cf81277d49ba20cbe725c2e691370a69
right42 <chjm452@naver.com>
2021-04-04 20:04:01 +0900

    대학강의 R study 4주차

---
+++ example3/Vector4.R
@@ -0,0 +1,9 @@
+score1 <- c(3, 5, 1, 4)
+score2 <- c(2, 3, 4, 6)
+
+print(score1 + score2)
+print(score1 - score2)
+print(sort(score1))
+
+print(sum(score1 < 4))
+print(score[score < 2 & score >= 1])
---
+++ example4/list.R
@@ -0,0 +1,10 @@
+score1 <- c(1, 2, 3, 45)
+score2 <- c(51, 2, 23, 45)
+score3 <- c(12, 22, 43, 45)
+
+scores <- list(score1, score2, score3)
+
+print(scores)
+
+student <- list(name = 'test', id = '123')
+print(student)
